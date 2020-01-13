cc03ba9ae1b31ea0b877f7677e6eb8684d634bf0
Halton Bagley <haltonba94@gmail.com>
2020-01-13 19:16:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,7 +1,17 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   data.each do |attributes, values|
-    
+    values.each do |types, names|
+      names.each do |name|
+        if !pigeon_list[name]
+          pigeon_list[name] = {}
+        end
+        if !pigeon_list[name][attributes]
+          pigeon_list[name][attributes] = []
+        end
+        pigeon_list[name][attributes] << values.to_s
+      end
+    end
   end
   pigeon_list
 end
